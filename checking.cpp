b594acbd1b9f52c67d2e0884451369aa1d30d5c2
adhanfirmansyah818@gmail.com <adhanfirmansyah818@gmail.com>
2026-04-01 15:40:01 +0700

    add void for menu

--- checking.cpp
+++ checking.cpp
@@ -55,4 +55,8 @@ void outputBilFibo(){    if (bilFibo(number)){
         cout<<number<< "Adalah bilangan Fibonacci" << endl;}
     else{
         cout<<number<< "Bukan bilangan Fibonacci" << endl;}
-}}
+}
+
+void menu(){
+    
+}
